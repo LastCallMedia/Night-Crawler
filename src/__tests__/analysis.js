9b12d3388b47f7978b0f7a0a0a5e38498620b40c
Rob Bayliss <rob@lastcallmedia.com>
2018-02-21 09:08:12 -0500

    Test cleanup

--- src/__tests__/analysis.js
+++ src/__tests__/analysis.js
@@ -2,16 +2,18 @@ import Analysis from '../analysis';
 import { Number } from '../metrics';
 
 describe('Analysis', function() {
+  var a;
+  beforeEach(function() {
+    a = new Analysis('Test', new Date);
+  })
   it('Should collect metrics', function() {
     var n = new Number('Test', 0, 1);
-    var a = new Analysis();
     a.addMetric('test', n);
     expect(a.metrics.size).toEqual(1);
     expect(a.metrics.get('test')).toEqual(n);
   });
 
   it('Should collect results', function() {
-    var a = new Analysis();
     a.addResult('foo', 0, 1, 'test');
     expect(a.results).toEqual([
       {
@@ -23,21 +25,32 @@ describe('Analysis', function() {
     ]);
   });
 
-  it('Should know if it has any failing results', function() {
-    var a = new Analysis();
-    expect(a.hasFailures()).toEqual(false);
-    a.addResult('warn', 1, 1, 'test');
-    expect(a.hasFailures()).toEqual(false);
-    a.addResult('fail', 2, 1, 'test');
-    expect(a.hasFailures()).toEqual(true);
-  });
+  it('Should have a name', function() {
+    expect(a.label).toBe('Test')
+  })
+  it('Should have a date', function() {
+    expect(a.date).toBeInstanceOf(Date)
+  })
 
-  it('Should know if it has any failing metrics', function() {
-    var a = new Analysis();
-    expect(a.hasFailures()).toEqual(false);
-    a.addMetric('warn', new Number('Warn', 1, 0));
-    expect(a.hasFailures()).toEqual(false);
-    a.addMetric('fail', new Number('Fail', 2, 0));
-    expect(a.hasFailures()).toEqual(true);
+  describe('hasFailures', function() {
+    it('Should not have failures on an empty analysis', function() {
+      expect(a.hasFailures()).toEqual(false);
+    });
+    it('Should not have failures on a warning result', function() {
+      a.addResult('warn', 1, 1, 'test');
+      expect(a.hasFailures()).toEqual(false);
+    });
+    it('Should have failures on a failure result', function() {
+      a.addResult('err', 2, 1, 'test');
+      expect(a.hasFailures()).toEqual(true);
+    });
+    it('Should not have failures on a warning metric', function() {
+      a.addMetric('warn', new Number('Warn', 1, 0));
+      expect(a.hasFailures()).toEqual(false);
+    });
+    it('Should have failures on a failing metric', function() {
+      a.addMetric('fail', new Number('Fail', 2, 0));
+      expect(a.hasFailures()).toEqual(true);
+    });
   });
 });
