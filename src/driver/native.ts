8a92f38521898e4be89a9e6612a9a4b05ffc5501
Rob Bayliss <rob@lastcallmedia.com>
2020-02-26 17:46:52 -0500

    Simplify driver class

--- src/driver/native.ts
+++ src/driver/native.ts
@@ -4,59 +4,43 @@ import http from 'http';
 import { URL } from 'url';
 import { performance } from 'perf_hooks';
 
-function getDriver(protocol: string): typeof http | typeof https {
-  switch (protocol) {
-    case 'http:':
-      return http;
-    case 'https:':
-      return https;
-    default:
-      throw new Error('Unknown protocol: ' + protocol);
-  }
-}
-
 type NativeDriverResponse = http.IncomingMessage & {
   time: number;
 };
 
-function request(
-  url: string,
-  options: https.RequestOptions
-): Promise<NativeDriverResponse> {
-  return new Promise((resolve, reject) => {
-    const parsed = new URL(url);
-    const driver = getDriver(parsed.protocol);
-    const theseOptions = Object.assign(
-      {
-        protocol: parsed.protocol.slice(0, -1),
-        host: parsed.hostname,
-        port: parsed.port,
-        path: parsed.pathname,
-        method: 'GET'
-      },
-      options
-    );
-
-    const start = performance.now();
-    const req = driver.request(theseOptions, res => {
-      resolve(Object.assign(res, { time: performance.now() - start }));
-    });
-    req.on('error', reject);
-    req.end();
-  });
-}
-
 export default class NativeDriver implements Driver<NativeDriverResponse> {
   opts: https.RequestOptions;
-  agent: http.Agent;
+  httpAgent: http.Agent;
+  httpsAgent: https.Agent;
   constructor(opts: https.RequestOptions = {}) {
     this.opts = opts;
-    this.agent = new http.Agent({ keepAlive: true });
+    this.httpAgent = new http.Agent({ keepAlive: true });
+    this.httpsAgent = new https.Agent({ keepAlive: true });
   }
 
-  fetch(req: CrawlRequest): Promise<NativeDriverResponse> {
-    return request(req.url, { ...this.opts, ...{ agent: this.agent } });
+  fetch(crawlRequest: CrawlRequest): Promise<NativeDriverResponse> {
+    return new Promise((resolve, reject) => {
+      const parsed = new URL(crawlRequest.url);
+      const theseOptions = Object.assign(
+        {
+          protocol: parsed.protocol,
+          host: parsed.hostname,
+          port: parsed.port,
+          path: parsed.pathname,
+          method: 'GET',
+          agent: this._getAgent(parsed)
+        },
+        this.opts
+      );
+      const start = performance.now();
+      const req = this._getDriver(parsed).request(theseOptions, res => {
+        resolve(Object.assign(res, { time: performance.now() - start }));
+      });
+      req.on('error', reject);
+      req.end();
+    });
   }
+
   collect(
     response: NativeDriverResponse
   ): { statusCode?: number; statusMessage?: string; time: number } {
@@ -65,5 +49,26 @@ export default class NativeDriver implements Driver<NativeDriverResponse> {
       statusMessage: response.statusMessage,
       time: response.time
     };
+  }
+
+  private _getAgent(url: URL): http.Agent {
+    switch (url.protocol) {
+      case 'https:':
+        return this.httpsAgent;
+      case 'http:':
+        return this.httpAgent;
+      default:
+        throw new Error('Unknown protocol: ' + url.protocol);
+    }
+  }
+  private _getDriver(url: URL): typeof http | typeof https {
+    switch (url.protocol) {
+      case 'https:':
+        return https;
+      case 'http:':
+        return http;
+      default:
+        throw new Error('Unknown protocol: ' + url.protocol);
+    }
   }
 }
