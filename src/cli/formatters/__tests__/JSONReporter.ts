39b2f5309074c696ecf2f5a4d08d7b4b73c13f59
Rob Bayliss <rob@lastcallmedia.com>
2020-03-17 18:08:38 -0400

    Fix ANSI codes making it into JSON

--- src/cli/formatters/__tests__/JSONReporter.ts
+++ src/cli/formatters/__tests__/JSONReporter.ts
@@ -14,6 +14,13 @@ describe('JSON Reporter', function() {
     pass: { pass: true },
     fail: { pass: false, message: 'there was an error.' }
   });
+  const ansi = makeResult({
+    ansi: {
+      pass: false,
+      message:
+        '\u001b[2mexpect(\u001b[22m\u001b[31mreceived\u001b[39m\u001b[2m)'
+    }
+  });
 
   it('Should write a json file containing results', async function() {
     const { path, cleanup } = await file();
@@ -23,6 +30,16 @@ describe('JSON Reporter', function() {
     reporter.report('mixed', mix);
     await expect(reporter.stop()).resolves.toBeUndefined();
     await expect(fs.readFile(path, 'utf8')).resolves.toMatchSnapshot();
+    await cleanup();
+  });
+
+  it('Should strip ansi codes from output', async function() {
+    const { path, cleanup } = await file();
+    const reporter = new JSONReporter(path);
+    reporter.report('ansi', ansi);
+    await reporter.stop();
+    const contents = JSON.parse(await fs.readFile(path, 'utf8'));
+    expect(contents[0].result.ansi.message).toBe('expect(received)');
     await cleanup();
   });
 });
