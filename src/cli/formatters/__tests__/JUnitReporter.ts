145571e65e87fb55cf79bd26e9f555807426bfbc
Rob Bayliss <rob@lastcallmedia.com>
2020-03-17 18:15:15 -0400

    Handle ansi output for junit too

--- src/cli/formatters/__tests__/JUnitReporter.ts
+++ src/cli/formatters/__tests__/JUnitReporter.ts
@@ -14,6 +14,13 @@ describe('JUnit Reporter', function() {
     pass: { pass: true },
     fail: { pass: false, message: 'there was an error.' }
   });
+  const ansi = makeResult({
+    ansi: {
+      pass: false,
+      message:
+        '\u001b[2mexpect(\u001b[22m\u001b[31mreceived\u001b[39m\u001b[2m)'
+    }
+  });
 
   it('Should write a junit file containing results', async function() {
     const { path, cleanup } = await file();
@@ -23,6 +30,16 @@ describe('JUnit Reporter', function() {
     reporter.report('mixed', mix);
     await expect(reporter.stop()).resolves.toBeUndefined();
     await expect(fs.readFile(path, 'utf8')).resolves.toMatchSnapshot();
+    await cleanup();
+  });
+
+  it('Should strip ANSI output', async function() {
+    const { path, cleanup } = await file();
+    const reporter = new JUnitReporter(path);
+    reporter.report('ansi', ansi);
+    await reporter.stop();
+    const contents = await fs.readFile(path, 'utf8');
+    await expect(contents).toContain('expect(received)');
     await cleanup();
   });
 });
