88f3d72afa7d2b3548d6397100344246aecc5425
Rob Bayliss <rob@lastcallmedia.com>
2020-03-19 09:37:37 -0400

    Better document example file

--- docs/example/nightcrawler.js
+++ docs/example/nightcrawler.js
@@ -5,6 +5,7 @@ const expect = require('expect');
 module.exports = crawl('Homepage', function() {
     const times = [];
 
+    // Tests run for every request/response cycle.
     test('Status code is 2xx', function(unit) {
        expect(unit.response.statusCode).toBeGreaterThanOrEqual(200);
        expect(unit.response.statusCode).toBeLessThan(300);
@@ -18,12 +19,16 @@ module.exports = crawl('Homepage', function() {
         times.push(unit.response.time);
     })
 
+    // After functions run after all responses have been received.
     after('Average response time should be < 200ms', function() {
         const sum = times.reduce((total, value) => total + value, 0);
         expect(sum / times.length).toBeLessThan(200);
     })
 
+    // Return any iterable/async iterable filled with request-shaped objects.
     return [
+        // driverOptions can be used to pass options to the driver.
+        // For example, passing {auth: 'foo:bar'} will enable basic auth.
         {url: 'https://www.example.com/', driverOptions: {}}
     ]
 })
